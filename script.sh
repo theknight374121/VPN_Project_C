6ec491601642df06c530de0cd486c5a3167ba1b3
5en5ei <theknight374121@gmail.com>
2016-04-17 18:07:06 -0700

    milestone 1 fixed with change in adapter settings

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 sudo ip addr add 10.0.21.1/24 dev toto0;
 sudo ifconfig toto0 up;
 sudo sysctl net.ipv4.ip_forward=1;
-sudo route add -net 10.0.3.0 netmask 255.255.255.0 dev toto0;
+sudo route add -net 192.168.67.0 netmask 255.255.255.0 dev toto0;
